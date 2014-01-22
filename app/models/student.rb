d1958b701b994c4f1940874cb04b236a110608e0
dbc-apprentice <apprentice@devbootcamp.com>
2014-01-22 13:22:42 -0600

    advanced avlidations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, :uniqueness => true, :format => { :with => /.+@.+\...+/ }
+  validates :age, :numericality => { :greater_than => 5 }
+  validates :pn_length, :numericality => { :greater_than => 9 }
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    ((Date.today - self.birthday) / 365).to_i
+  end
+
+  def pn_length
+    self.phone.gsub(/\D/, '').length
+  end
 end
